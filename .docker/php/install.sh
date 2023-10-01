5d5c9a9f79a426cb6ec82e16b04b6dae8d168473
ismail <ismail@logitrade.nl>
2023-10-01 20:29:21 +0200

    update

--- .docker/php/install.sh
+++ .docker/php/install.sh
@@ -3,29 +3,26 @@ rm -rf ./*
 echo "-------------------------------------------------------------------"
 echo "-             Laminas MVC Skeleton Application                    -"
 echo "-------------------------------------------------------------------"
-composer create-project -s dev laminas/laminas-mvc-skeleton --no-interaction web
+composer create-project -s dev laminas/laminas-mvc-skeleton --no-interaction my_project
 
 echo "-------------------------------------------------------------------"
 echo "-                   require packages                              -"
 echo "-------------------------------------------------------------------"
-cd ./web
-
-#symfony composer config extra.symfony.allow-contrib true
-#symfony composer config allow-plugins.symfony/runtime true
+cd ./my_project
 symfony composer require php:^8.2.0
 
 composer require --no-interaction laminas/laminas-cli laminas/laminas-session
 composer require --no-interaction doctrine/doctrine-orm-module --with-all-dependencies
+
 echo "-------------------------------------------------------------------"
 echo "-               require dev packages                              -"
 echo "-------------------------------------------------------------------"
-
 symfony composer require --dev --no-interaction laminas/laminas-developer-tools
 
 rm -rf .git
 cd ..
 
-chmod -R a+rw web
+chmod -R a+rw my_project
 
-rsync -a web/ ./
-rm -rf web
+rsync -a my_project/ ./
+rm -rf my_project
